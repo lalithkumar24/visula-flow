cd370ddcd1e2d8e42aa86fce63aeb002b84b8bfd
lalithkumar24 <lali22048.it@rmkec.ac.in>
2025-03-31 21:31:43 +0000

    impemented sidebar and 404 page

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Separator } from "@/components/ui/separator";
+import DisktopSidebar from "@/components/Sidebar";
+import BreadcrumbHeader from "@/components/BreadcrumbHeader";
+import { ModeToggle } from "@/components/TheamModeToggle";
+const layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="flex h-screan">
+      <DisktopSidebar />
+      <div className="flex flex-col flex-1 min-h-screen">
+        <header className="flex items-center justify-between px-6 py-4 h-[50px] container">
+          <BreadcrumbHeader />
+          <div className="gap-1 flex items-center"><ModeToggle/></div>
+        </header>
+        <Separator />
+        <div className="overflow-auto">
+          <div className="flex-1 container py-4 text-accent-foreground">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default layout;
